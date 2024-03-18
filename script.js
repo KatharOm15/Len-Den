da79e6c734aa9df7fcbb32e4ee7ce235fd3bbfc3
om kathar <omkathar15@gmail.com>
2024-03-18 17:42:05 +0530

    All Category Added

--- script.js
+++ script.js
@@ -1,3 +1,60 @@
+//cards slider with click
+document.addEventListener("DOMContentLoaded", function () {
+  const restaurantContainer = document.querySelector(".card-slider");
+  const leftRButton = document.querySelector(".restaurant-arrow-left");
+  const rightRButton = document.querySelector(".restaurant-arrow-right");
+
+  function updateButtonState() {
+    console.log("Clicked");
+    leftRButton.disabled = restaurantContainer.scrollLeft <= 0;
+    rightRButton.disabled =
+      restaurantContainer.scrollLeft + restaurantContainer.offsetWidth >=
+      restaurantContainer.scrollWidth;
+  }
+
+  console.log("Clicked1");
+  updateButtonState();
+
+  leftRButton.onclick = function () {
+    restaurantContainer.scrollBy({
+      left: -restaurantContainer.offsetWidth / 2,
+      behavior: "smooth",
+    });
+  };
+
+  rightRButton.onclick = function () {
+    restaurantContainer.scrollBy({
+      left: restaurantContainer.offsetWidth / 2,
+      behavior: "smooth",
+    });
+  };
+  restaurantContainer.addEventListener("scroll", updateButtonState);
+});
+
+//Slider JS (Dashboard)
+document.addEventListener("DOMContentLoaded", function () {
+  const slides = document.querySelector(".slides");
+  const slideWidth = slides.firstElementChild.clientWidth;
+  let currentPosition = 0;
+  let currentSlideIndex = 0;
+  const numSlides = slides.children.length;
+
+  function slide() {
+    currentPosition -= slideWidth;
+    currentSlideIndex++;
+
+    if (currentSlideIndex >= numSlides) {
+      currentPosition = 0;
+      currentSlideIndex = 0;
+    }
+
+    slides.style.transition = "transform 0.5s ease-in-out";
+    slides.style.transform = `translateX(${currentPosition}px)`;
+  }
+
+  setInterval(slide, 3000);
+});
+
 //Login page
 const inputField = document.getElementById("username");
 
